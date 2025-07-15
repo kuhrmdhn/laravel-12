6e20325ea70f3fb3f6d75d865a46e885aa95e977
Kukuh Ardi Ramadhan <kukuhardi669@gmail.com>
2025-07-15 21:09:29 +0700

    [route] routing, parsing data and wildcard

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$name}} About Page</title>
+</head>
+
+<body>
+    <h1>
+        About Page
+    </h1>
+    <p>Name: {{$name}}</p>
+    <p>Age: {{$age}}</p>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Nested route file
+Route::get('/blog/html', function() {
+    return view("blog.html");
+});
+
+
+// Parsing Data
+// 1. With array
+Route::get('/about', function () {
+    $name = "Kukuh Ardi Ramadhan";
+    $birthYear = 2005;
+    $currentYear = gmdate('Y');
+    $age = $currentYear - $birthYear;
+    return view('about', ['name' => $name, 'age' => $age]);
+});
+// 2/ Using compact()
+Route::get('/contact', function () {
+    $instagram = "https://www.instagram.com/kuh.rmdhn/";
+    return view("contact", compact('instagram'));
+});
+// 3. Using with()
+Route::get('/personal-web', function () {
+    $personalWebUrl = "https://kuhrmdhn.vercel.app";
+    return view('personalWeb')->with('personalWebUrl', $personalWebUrl);
+});
+
+// Wildcard
+// Dynamic route using wildcard
+Route::get('/project/{projectId}', function ($projectId) {
+    return view("project", compact('projectId'));
+});
